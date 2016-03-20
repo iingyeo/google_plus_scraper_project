e1f5ea79684a7a95c49d7d870aa0750c6086aeda
taemyung <tmheo74@gmail.com>
2016-03-20 23:21:02 +0900

    #3 implement subscribe form to receive email

--- google_plus_scraper_site/google_plus_scraper_app/admin.py
+++ google_plus_scraper_site/google_plus_scraper_app/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import GooglePlusArticle
+from .models import GooglePlusArticle, GooglePlusArticleSubscriber
 
 # Register your models here.
 class GooglePlusArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'link', 'date')
+    search_fields = ('title', 'link')
 admin.site.register(GooglePlusArticle, GooglePlusArticleAdmin)
+
+class GooglePlusArticleSubscriberAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created')
+    search_fields = ('name', 'email')
+admin.site.register(GooglePlusArticleSubscriber, GooglePlusArticleSubscriberAdmin)
