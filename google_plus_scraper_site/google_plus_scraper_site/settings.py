c58c0367c5133852e1f5a739962774584bf2aa80
taemyung <tmheo74@gmail.com>
2016-03-25 23:18:26 +0900

    #2 #4 implement scheduling sending email to subscriber

--- google_plus_scraper_site/google_plus_scraper_site/settings.py
+++ google_plus_scraper_site/google_plus_scraper_site/settings.py
@@ -127,3 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Email Setting
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'test@test.com'
+EMAIL_HOST_PASSWORD = 'test'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
